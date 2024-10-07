a6d10e00989936715ebc556c653e43ab31633cad
이민규 <67575226+suslmk-lee@users.noreply.github.com>
2024-10-07 18:01:14 +0900

    API_BASE_URL 값 수정 / localhost 제거

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import './App.css';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080'
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
 
 const App = () => {
     const [data, setData] = useState([]);
